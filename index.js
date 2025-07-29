e2601dad81f7b7f02d250aed6583d605e4885a56
Fergrenteski <luizgrenfer@gmail.com>
2025-07-28 22:25:29 -0300

    cors

--- index.js
+++ index.js
@@ -10,17 +10,12 @@ const app = express();
 const allowedOrigins = [process.env.FRONTEND_URL];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true, // se estiver usando cookies ou auth com headers
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 // Configuração para aumentar o limite de payload
 app.use(express.json({ limit: '10mb' }));
